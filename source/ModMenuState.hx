e26a87b687898c2057f3b3b3f71868264d4569c3
Thejoojguy <91154654+Thejoojguy@users.noreply.github.com>
2021-09-21 21:30:03 -0300

    Update ModMenuState.hx

--- source/ModMenuState.hx
+++ source/ModMenuState.hx
@@ -25,7 +25,8 @@ class ModMenuState extends MusicBeatState
 
 	var weekData:Array<Dynamic> = [
 		['Headache', 'Nerves', 'Release', 'Fading'],
-		['Cackle', 'Bones', 'Mystic', 'Hocus-Pocus'],			
+		['Cackle', 'Bones', 'Mystic', 'Hocus-Pocus'],
+                ['foolhardy'],			
 	];
 	var curDifficulty:Int = 1;
 
@@ -33,12 +34,14 @@ class ModMenuState extends MusicBeatState
 
 	var weekCharacters:Array<Dynamic> = [
 		['garcello', 'bf', 'gf'],
-		['dad', 'bf', 'gf'],									
+		['dad', 'bf', 'gf'],
+		['dad', 'bf', 'gf'],							
 	];
 
 	var weekNames:Array<String> = [
 		"SMOKE 'EM OUT STRUGGLE",
-		"Tales from the Raveyard",	
+		"Tales from the Raveyard",
+    	        "zardy pog!",
 	];
 
 	var txtWeekTitle:FlxText;
